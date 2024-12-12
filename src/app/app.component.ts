2e7486d2639c27d16dbdc66a51705939e2801713
Junior Morais <jrmorais3@gmail.com>
2024-12-12 05:20:41 -0300

    adding a filter feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./components/shared/nav/nav.component";
 import { HeaderComponent } from "./components/shared/header/header.component";
 import { FooterComponent } from "./components/shared/footer/footer.component";
+import { MagicReadsComponent } from "./components/magic-reads/magic-reads.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, NavComponent, HeaderComponent, FooterComponent, MagicReadsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/magic-reads/magic-reads.component.ts
+++ src/app/components/magic-reads/magic-reads.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FiltersComponent } from "./filters/filters.component";
+import { BooksComponent } from "./books/books.component";
 
 @Component({
   selector: 'app-magic-reads',
-  imports: [],
+  imports: [FiltersComponent, BooksComponent],
   templateUrl: './magic-reads.component.html',
+  standalone: true,
   styleUrl: './magic-reads.component.css'
 })
 export class MagicReadsComponent {
