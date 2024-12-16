c025a9e19d8441733fbba86b9fbc49929e0a97a2
Junior Morais <jrmorais3@gmail.com>
2024-12-16 05:11:34 -0300

    saving work on books-service

--- src/app/components/magic-reads/books/books.component.service.ts
+++ src/app/components/magic-reads/books/books.component.service.ts
@@ -1,2 +1,7 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Book } from "./book/model/Book";
 
+@Injectable()
+
+export class BooksService{}
